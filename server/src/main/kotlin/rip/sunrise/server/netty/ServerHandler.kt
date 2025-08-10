204a537a8917cae3d127d2d67baa89c03b90d7e7
Sunderw <73128678+Sunderw3k@users.noreply.github.com>
2025-08-10 18:02:50 -0400

    fix: correct response for a packet

--- server/src/main/kotlin/rip/sunrise/server/netty/ServerHandler.kt
+++ server/src/main/kotlin/rip/sunrise/server/netty/ServerHandler.kt
@@ -94,8 +94,8 @@ class ServerHandler(private val config: Config, private val http: JarHttpServer)
 
             is ScriptStartRequest -> ctx.writeAndFlush(ScriptStartResp(false))
 
-            is a5 -> ctx.writeAndFlush(am(6))
-            
+            is a5 -> ctx.writeAndFlush(am(0))
+
             is aY -> ctx.writeAndFlush(bs(USER_ID))
 
             is GetActiveInstancesRequest -> ctx.writeAndFlush(GetInstancesResp(0))
