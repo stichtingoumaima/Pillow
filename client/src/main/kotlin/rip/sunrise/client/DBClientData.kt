37a8ced4b5359b608403b81fac063f36dd3a533e
Sunderw <73128678+Sunderw3k@users.noreply.github.com>
2025-08-14 08:08:37 -0400

    fix: update shared secret for 3.30.30

--- client/src/main/kotlin/rip/sunrise/client/DBClientData.kt
+++ client/src/main/kotlin/rip/sunrise/client/DBClientData.kt
@@ -49,7 +49,7 @@ object DBClientData {
             val data = extractData(bytes)
             val version = data.first ?: error("Failed to find version!")
 //            val sharedSecret = data.second ?: error("Failed to find shared secret!")
-            val sharedSecret = "4ebbdd702648a0122edbcc"
+            val sharedSecret = "c60839b9c37fcd22e8785a"
 
             val clientCache = ClientCache(version, hash, sharedSecret, conn.getHeaderField("etag"))
 
