08ce2e8ed8e4d2e055bfe3702baa145e5caccf6f
Sunderw <73128678+Sunderw3k@users.noreply.github.com>
2025-08-14 13:12:07 -0400

    fix: agent workaround for 3.30.30
    As netty got obfuscated, simple addLast hooks are no more.
    It isn't that bad though. I can most likely write an auto-dumper for the names. Or think of some JDK exclusive bypass, though unlikely due to openssl.

--- agent/src/main/kotlin/rip/sunrise/agent/Main.kt
+++ agent/src/main/kotlin/rip/sunrise/agent/Main.kt
@@ -9,7 +9,6 @@ import rip.sunrise.injectapi.hooks.inject.InjectHook
 import rip.sunrise.injectapi.hooks.inject.modes.HeadInjection
 import rip.sunrise.injectapi.managers.HookManager
 import java.lang.instrument.Instrumentation
-import java.lang.reflect.Method
 import java.net.InetSocketAddress
 import java.net.URL
 
@@ -74,12 +73,11 @@ fun premain(args: String?, inst: Instrumentation) {
             HeadInjection(),
             Class.forName("org.dreambot.api.script.ScriptManager"),
             TargetMethod("hasPurchasedScript", "(I)Z"),
-            listOf(CapturedArgument(Opcodes.ALOAD, 0), CapturedArgument(Opcodes.ILOAD, 1))
-        ) { ctx: Context, instance: Any, productId: Int ->
+            listOf(CapturedArgument(Opcodes.ILOAD, 1))
+        ) { ctx: Context, productId: Int ->
             log("Checking whether we purchased product $productId")
             if (productId != ANTISPOOF_PRODUCT_CONSTANT && productId > 0) {
                 ctx.setReturnValue(true)
-                return@InjectHook
             }
         }
     )
@@ -89,12 +87,11 @@ fun premain(args: String?, inst: Instrumentation) {
             HeadInjection(),
             Class.forName("org.dreambot.api.script.ScriptManager"),
             TargetMethod("hasPremiumScript", "(I)Z"),
-            listOf(CapturedArgument(Opcodes.ALOAD, 0), CapturedArgument(Opcodes.ILOAD, 1))
-        ) { ctx: Context, instance: Any, productId: Int ->
+            listOf(CapturedArgument(Opcodes.ILOAD, 1))
+        ) { ctx: Context, productId: Int ->
             log("Checking whether we have premium script $productId")
             if (productId != ANTISPOOF_PREMIUM_CONSTANT && productId > 0) {
                 ctx.setReturnValue(true)
-                return@InjectHook
             }
         }
     )
@@ -104,61 +101,60 @@ fun premain(args: String?, inst: Instrumentation) {
             HeadInjection(),
             Class.forName("org.dreambot.api.script.ScriptManager"),
             TargetMethod("hasSDNScript", "(I)Z"),
-            listOf(CapturedArgument(Opcodes.ALOAD, 0), CapturedArgument(Opcodes.ILOAD, 1))
-        ) { ctx: Context, instance: Any, productId: Int ->
+            listOf(CapturedArgument(Opcodes.ILOAD, 1))
+        ) { ctx: Context, productId: Int ->
             log("Checking whether we have SDN script $productId")
             if (productId != ANTISPOOF_SDN_CONSTANT && productId > 0) {
                 ctx.setReturnValue(true)
-                return@InjectHook
             }
         }
     )
+    HookManager.addHook(
+        InjectHook(
+            HeadInjection(),
+            InetSocketAddress::class.java,
+            TargetMethod("createUnresolved", "(Ljava/lang/String;I)Ljava/net/InetSocketAddress;"),
+            listOf(CapturedArgument(Opcodes.ALOAD, 0), CapturedArgument(Opcodes.ILOAD, 1))
+        ) { ctx: Context, host: String, port: Int ->
+            if (host == "cdn.dreambot.org" && port == 43831) {
+                println("Redirecting netty from $host:$port to $SERVER_HOST:$SERVER_PORT")
+                ctx.setReturnValue(InetSocketAddress.createUnresolved(SERVER_HOST, SERVER_PORT))
+            }
+        })
 
-    // TODO: Hey, DreamBot owners - please bump the netty version to not use java 6.
     var fakeSslHandler: Any? = null
     HookManager.addHook(
         InjectHook(
             HeadInjection(),
-            Method::class.java,
-            TargetMethod("invoke", "(Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object;"),
-            listOf(
-                CapturedArgument(Opcodes.ALOAD, 0),
-                CapturedArgument(Opcodes.ALOAD, 1),
-                CapturedArgument(Opcodes.ALOAD, 2)
-            )
-        ) { ctx: Context, method: Method, instance: Any?, args: Array<Any>? ->
-            // Don't set the SslHandler
-            if (method.declaringClass.name == "io.netty.channel.ChannelPipeline" && method.name == "addLast") {
-                // NOTE: For some reason it's an array, not a single handler.
-                val handlers = args!![0]
-                if (!handlers.javaClass.isArray) return@InjectHook
+            Class.forName("org.5C"),
+            TargetMethod("14", "([Ljava/lang/Object;)Lorg/7A;"),
+            listOf(CapturedArgument(Opcodes.ALOAD, 0), CapturedArgument(Opcodes.ALOAD, 1))
+        ) { ctx: Context, instance: Any, args: Array<Any> ->
+            val handlers = args[0] as Array<*>
+            val handler = handlers[0] as Any
 
-                val handler = (handlers as Array<*>)[0]!!
-                if (!Class.forName("io.netty.handler.ssl.SslHandler").isInstance(handler)) return@InjectHook
+            println("addLast call for handler!")
+            println(handler.javaClass)
 
+            if (handler.javaClass.name == "org.0T") {
+                println("Found SSL Handler")
                 fakeSslHandler = handler
                 ctx.setReturnValue(instance)
             }
+        })
 
-            // Spoof SslHandler
-            else if (method.declaringClass.name == "io.netty.channel.ChannelPipeline" && method.name == "get") {
-                val handlerType = args!![0] as Class<*>
-                if (handlerType.name != "io.netty.handler.ssl.SslHandler") return@InjectHook
+    HookManager.addHook(
+        InjectHook(
+            HeadInjection(),
+            Class.forName("org.5C"),
+            TargetMethod("9D", "([Ljava/lang/Object;)Lorg/6V;"),
+            listOf(CapturedArgument(Opcodes.ALOAD, 1))
+        ) { ctx: Context, args: Array<Any> ->
+            val clazz = args[0] as Class<*>
+
+            if (clazz.name == "org.0T") {
+                println("Get for SSL Handler")
                 ctx.setReturnValue(fakeSslHandler)
-            }
-
-            // Redirect the connection
-            else if (method.declaringClass.name == "io.netty.bootstrap.Bootstrap" && method.name == "connect") {
-                val origHost = args!![0]
-                val origPort = args[1]
-
-                println("Redirecting netty from $origHost:$origPort to $SERVER_HOST:$SERVER_PORT")
-
-                val future = Class.forName("io.netty.bootstrap.Bootstrap")
-                    .getDeclaredMethod("connect", String::class.java, Int::class.java)
-                    .invoke(instance, SERVER_HOST, SERVER_PORT)
-
-                ctx.setReturnValue(future)
             }
         })
 
