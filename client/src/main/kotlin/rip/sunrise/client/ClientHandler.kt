stichtingoumaima/Pillow

commit 14a1d6ce3e73b91164dddc56161acc5c9c236d7c

fix: improve headers on http requests
This whole thing needs a rewrite to be fair. It's not like the actual protocol at all.

client/src/main/kotlin/rip/sunrise/client/ClientHandler.kt
@@ -127,11 +127,13 @@ class ClientHandler(val username: String, val password: String, val hardwareId: 
             }
 
             is EncryptedScriptResp -> {
+                val request = HttpRequest.newBuilder(URI(msg.w))
+                    .header("If-match", "\"${msg.l}\"")
+                    .header("User-agent", "Java/11.0.25")
+                    .build()
+
                 val encrypted = HttpClient.newHttpClient()
-                    .send(
-                        HttpRequest.newBuilder(URI(msg.w)).build(),
-                        HttpResponse.BodyHandlers.ofInputStream()
-                    )
+                    .send(request, HttpResponse.BodyHandlers.ofInputStream())
                     .body()
                     .readAllBytes()
 

commit 91143e9142ef9eea9a3ac4cc4ec772e1bd783f26

fix: make unencrypted scripts dumpable (caused by 14a1d6ce3e73b91164dddc56161acc5c9c236d7c)
The value might be null. Should only send an If-Match if it isn't.

client/src/main/kotlin/rip/sunrise/client/ClientHandler.kt
@@ -114,13 +114,15 @@ class ClientHandler(val username: String, val password: String, val hardwareId: 
     }
 
     fun getScriptBytes(msg: EncryptedScriptResp): ByteArray {
-        val request = HttpRequest.newBuilder(URI(msg.w))
-            .header("If-match", "\"${msg.l}\"")
+        val builder = HttpRequest.newBuilder(URI(msg.w))
             .header("User-agent", "Java/11.0.25")
-            .build()
+
+        msg.l?.let {
+            builder.header("If-match", "\"$it\"")
+        }
 
         val encrypted = HttpClient.newHttpClient()
-            .send(request, HttpResponse.BodyHandlers.ofInputStream())
+            .send(builder.build(), HttpResponse.BodyHandlers.ofInputStream())
             .body()
             .readAllBytes()
 